ce913cefa07735940e6ac088a68f5096b90b12da
Taylor Smart <taylorsmart@Taylors-MacBook-Pro.local>
2021-03-20 15:52:57 -0600

    adjustments to schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- DROP DATABASE IF EXISTS product_information
 
 -- CREATE DATABASE product_information
@@ -48,8 +47,8 @@ DROP TABLE IF EXISTS photo_list CASCADE;
 CREATE TABLE photo_list (
   id INTEGER NOT NULL  ,
   style_id INTEGER,
-  thumbnail_url VARCHAR(1000),
-  photo_url VARCHAR(1000),
+  thumbnail_url TEXT,
+  photo_url TEXT,
   PRIMARY KEY (id),
   CONSTRAINT fk_style_id 
     FOREIGN KEY(style_id)
@@ -80,97 +79,6 @@ CREATE TABLE related (
       REFERENCES product(id)
 );
 
-=======
--- CREATE DATABASE chat; /* Consoling this out as we can see the chat database by using `use chat`*/
-
-USE chat;
-
--- -- CREATE TABLE messages (
--- --   /* Describe your table here.*/
--- -- );
-
--- -- CREATE TABLE test_table (
--- --   id int,
--- --   col1 varChar(256),
--- --   col2 int,
--- --   col3 int,
--- --   PRIMARY KEY (id)
--- --   /* Describe your table here.*/
--- -- );
-
--- -- CREATE TABLE test_table2 (
--- --   id int,
--- --   col1 varChar(256),
--- --   col2 int,
--- --   col3 int,
--- --   PRIMARY KEY (id)
---   /* Describe your table here.*/
--- -- );
-
--- /* Create other tables and define schemas for them here! */
--- ------------------------------------------------------
--- ------------------------------------------------------
--- ------------------------------------------------------
-
--- ---
--- Table 'users'
---
--- ---
-
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users` (
-  `id` INTEGER ,
-  `github_handle` VARCHAR(256),
-  `username` VARCHAR(256),
-  unique (`username`),
-  PRIMARY KEY (`id`, `username`)
-);
-
--- ---
--- Table 'rooms'
---
--- ---
-
-DROP TABLE IF EXISTS `rooms`;
-
-CREATE TABLE `rooms` (
-  `id` INTEGER,
-  `roomname` VARCHAR(256),
-  unique (`roomname`),
-  PRIMARY KEY (`id`, `roomname`)
-);
-
--- -- ---
--- -- Table 'messages'
--- --
--- -- ---
-
-DROP TABLE IF EXISTS `messages`;
-
-CREATE TABLE `messages` (
-  `id` INTEGER ,
-  `text` VARCHAR(256) ,
-  `github_handle` VARCHAR(256) ,
-  `sent_time` INTEGER,
-  `roomname` VARCHAR(256),
-  `username` VARCHAR(256),
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`roomname`)
-    REFERENCES rooms(`roomname`),
-  FOREIGN KEY (`username`)
-    REFERENCES users(`username`)
-);
-
-
--- ---
--- Foreign Keys
--- ---
-
-
-
-
->>>>>>> dbefecdcf59fd8c16c00fd93a9f509ca43050d2d
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
